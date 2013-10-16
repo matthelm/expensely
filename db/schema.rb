e4370192e58acaa3ab05a48b4c79ec3059b8101f
Matt Helm <matt@matthelm.ca>
2013-10-15 22:54:58 -0400

    Models (ruby)

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131009005634) do
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "expenses", force: true do |t|
+    t.decimal  "total_amount"
+    t.string   "currency"
+    t.decimal  "total_cad"
+    t.decimal  "total_tax"
+    t.integer  "category_id"
+    t.date     "incurred_at"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "expenses", ["category_id"], name: "index_expenses_on_category_id"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = [
+  "Transportation (Airfare, car rentals, parking & mileage)",
+  "Accommodations (Hotel rooms)",
+  "Meals & Entertainment",
+  "Misc. Travel Expenses",
+  "Professional Development (Conference & course fees)",
+  "Recruitment Costs",
+  "Cellphones & Wireless",
+  "Software & Licenses",
+  "Computer Hardware",
+  "Computer Supplies (Peripherals & add-ons)",
+]
+
+categories.each do |c|
+  Category.create(title: c)
+end
+
+category_id = Category.first.id
+
+expenses = [
+  {
+    total_amount: 65,
+    total_cad: 65,
+    total_tax: 3.5,
+    currency: 'CAD',
+    incurred_at: '2013-09-10',
+    description: 'Taxi to airport',
+    category_id: category_id
+  },
+  {
+    total_amount: 125.32,
+    total_cad: 52,
+    total_tax: 15.2,
+    currency: 'CAD',
+    incurred_at: '2013-09-12',
+    description: 'Dinner for recruiting',
+    category_id: category_id
+  },
+  {
+    total_amount: 52,
+    total_cad: 52,
+    total_tax: 4.1,
+    currency: 'USD',
+    incurred_at: '2013-10-02',
+    description: 'Mike\'s D3 Book',
+    category_id: category_id
+  }
+]
+
+expenses.each do |e|
+  Expense.create(e)
+end
